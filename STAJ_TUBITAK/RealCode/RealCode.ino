32e95b6c99a62396c3c0601aa7ba4b446331a67a
MelisaSarici <sarici.asena@gmail.com>
2017-08-22 17:27:49 +0200

    Code Trial on the PCB

--- STAJ_TUBITAK/RealCode/RealCode.ino
+++ STAJ_TUBITAK/RealCode/RealCode.ino
@@ -1,48 +1,68 @@
+int Current = A0;
+int Voltage = A1;
+      
+      
+      float D=0.00;
+      float I;
+      float V;
+      float Increment_value=1.00;
+      unsigned long start=0;
 
 
+      
 void setup() {
   Serial.begin(9600);
+  pinMode(Current,INPUT);
+  pinMode(Voltage,INPUT);
  
 }
 
 void loop() {
-  float DUTYcycle=power();
-  analogWrite(5,DUTYcycle);
-
+  float D=power();
+  analogWrite(3,D);
 }
 
 
 
 float power(){
-      int T= 1000;
+       float Pmax=0;
+       float P;
+      unsigned long T= 2000;
       unsigned long last_t=millis();
-      int Current = A0;
-      int Voltage = A1;
-      float D=0;
-      float P;
-      float I;
-      float V;
-      float Increment_value=0.01;
-      int Pmax=0;
-      float DC;
-      
-      if (last_t==T){
-        I=analogRead(Current);
-        V=analogRead(Voltage);
+     if((last_t-start)>=T){
+        start=last_t;
+        I=analogRead(Current)*4.50/1024.00;
+        V=analogRead(Voltage)*4.50/1024.00;
         P=I*V;
-        Serial.print("Power of the system:");
+        Serial.print("Power of the system: ");
         Serial.println(P);
-        
-          if ((D>=0)&&(D<1)){
-            if(P>Pmax){
-              D==D+Increment_value;
+        Serial.print("Battery Voltage is ");
+        Serial.println(V ,"V");
+        Serial.print("Battery Current is ");
+        Serial.println(I, "A");
+        Serial.println();
+
+    if ((V<4.3)&&(I<0.5)){
+          if ((D>=0.00)&&(D<255.00)){
+            if(P>=Pmax){
+              D=D+Increment_value;
+              Pmax=P;
+              Serial.println(D);
+              Serial.println("***");
+
           }
             else if (P<Pmax){
-              D==D-Increment_value;
+              D=D-Increment_value;
+              Serial.println(D);
+              Serial.println("***");
+
+              
               }
           }
-          DC=D*255;
-          return DC;   
+          Serial.println(Pmax); 
+          return D;
+            
   
-  }
+    }
+      }
 }
